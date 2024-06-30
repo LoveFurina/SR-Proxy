ca849ce2f855e7f1764f3baa4fa45c1db2009304
LoveFurina <154606709+LoveFurina@users.noreply.github.com>
2024-06-30 20:13:58 +0800

    首次commit

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System.Net;
+
+namespace SR.Tool.Proxy
+{
+    internal static class Program
+    {
+        private const string Title = "SR 代理";
+
+        private static ProxyService s_proxyService;
+        private static EventHandler s_processExitHandler = new EventHandler(OnProcessExit);
+        
+        private static void Main(string[] args)
+        {
+            Console.Title = Title;
+            CheckProxy();
+
+            s_proxyService = new ProxyService("127.0.0.1", 21000);
+            AppDomain.CurrentDomain.ProcessExit += s_processExitHandler;
+
+            Thread.Sleep(-1);
+        }
+
+        private static void OnProcessExit(object sender, EventArgs args)
+        {
+            s_proxyService.Shutdown();
+        }
+
+        public static void CheckProxy()
+        {
+            try
+            {
+                string ProxyInfo = GetProxyInfo();
+                if (ProxyInfo != null)
+                {
+                    Console.WriteLine("嗯...看来您正在使用其他代理软件（例如Clash，V2RayN，Fiddler等）)");
+                    Console.WriteLine($"您的系统代理: {ProxyInfo}");
+                    Console.WriteLine("您必须关闭所有其他代理软件才能确保 SR.Tool.Proxy 能够正常工作。");
+                    Console.WriteLine("如果您关闭了其他代理软件，或者您认为您没有使用其他代理，请按任意键继续。");
+                    Console.ReadKey();
+                }
+            }
+            catch (NullReferenceException)
+            {
+
+            }
+        }
+
+        public static string GetProxyInfo()
+        {
+            try
+            {
+                IWebProxy proxy = WebRequest.GetSystemWebProxy();
+                Uri proxyUri = proxy.GetProxy(new Uri("https://www.example.com"));
+
+                string proxyIP = proxyUri.Host;
+                int proxyPort = proxyUri.Port;
+                string info = proxyIP + ":" + proxyPort;
+                return info;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # SR-Proxy
  代理转向工具
+
+本人一键端所使用的代理，派生自Tart的FreeSR
+
+默认会将流量重定向至 `localhost:21000`
+# 编译
+### 需求
+.NET 7.0 SDK
+```shell
+git clone https://github.com/LoveFurina/SR-Proxy
+cd SR-Proxy
+dotnet build
+```
+编译完的程序可在 bin 文件夹里找到
+# 说明
+- 程序使用系统代理进行流量重定向，会与其它代理软件的系统代理模式冲突(TUN模式不受影响)
+- 若要用它代理LC等部分服务端，记得关闭 `httpServer` 的 HTTPS/SSL 功能并修改端口
